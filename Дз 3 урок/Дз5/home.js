421521f5f3b3a5d03f641e0843265dc437e87723
muxus-fd <65289969+muxus-fd@users.noreply.github.com>
2020-07-22 09:56:10 +0300

    Дз к 3 уроку

---
+++ Дз 3 урок/Дз5/home.js
@@ -0,0 +1,39 @@
+"use strict";
+const products = [
+    {
+            id: 3,
+            price: 127,
+            photos: [
+                 "1.jpg",
+                 "2.jpg", 
+                    ]
+    }, 
+    {
+            id: 5,
+            price: 499,
+            photos: []
+    }, 
+    { 
+            id: 10,
+            price: 26,
+            photos: [
+                "3.jpg"
+                    ]
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+// вывод объектов с фото
+const result = products.filter(function(element) {
+            return ("photos" in element  && element.photos.length > 0 )});
+console.log(result);
+
+//сортировка по цене
+const sortPrice = products.sort(function(a, b) {
+   return a.price - b.price;
+})
+console.log(sortPrice)
+
+
---
+++ Дз 3 урок/Дз7/home.js
@@ -0,0 +1,8 @@
+"use strict";
+    
+let result = "x";
+for (let i = 0; i < 20; i++){
+    console.log(result);
+    result = result + "x";
+}
+    
